314e45c8e3da2ba189dd47b3c86df34c06e20112
Sibgatulina Rozalia Ahzymovna <rasibgatulina@gmail.com>
2020-05-13 20:06:42 +0500

    - 1 -

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# test2
+Docker and docker-compose class
+===============================
+
+Note about working user variables: you should export two variables at host machine — `DUID` (docker user id), variable with your current user ID and `DGID` (docker group id), variable with your current group.
+
+These variables are uses for launch php-process in a container.
+
+For example:    
+```shell script
+export DUID=$(id -u) && export DGID=$(id -g)
+```
+
+## Run project locally
+
+1. Build project: 
+    ```shell script
+    docker-compose build
+    ```
+1. Launch project:
+    ```shell script
+    docker-compose up -d
+    ```
+1. Install packages:
+    ```shell script
+    docker-compose exec app composer install
+    ```
+1. Run tests:
+    ```shell script
+    docker-compose exec app vendor/bin/phpunit
+    ```
+
+After start docker-compose you will see in your browser the next one:
+
+![Screenshot_2020-05-12_at_15.12.54](/uploads/0ae4961e86a2bd8ddd5472de03351a71/Screenshot_2020-05-12_at_15.12.54.png)
+
+This is a page with information about server environment variables, request and response data.
---
+++ src/MainController.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+use Symfony\Component\HttpFoundation\{Request, Response};
+use Symfony\Component\Serializer\{Encoder\JsonEncode, SerializerInterface};
+use Twig\Environment;
+use Twig\Error\{LoaderError, RuntimeError, SyntaxError};
+
+/**
+ * Class MainController
+ */
+class MainController
+{
+    private const QUOTE = <<<TXT
+I have a dream that one day, down in Alabama, with its vicious racists, with its governor having his lips dripping with the words of "interposition" and "nullification"
+TXT;
+
+    private SerializerInterface $serializer;
+    private Environment $twig;
+
+    /**
+     * @param SerializerInterface $serializer
+     * @param Environment $twig
+     */
+    public function __construct(SerializerInterface $serializer, Environment $twig)
+    {
+        $this->serializer = $serializer;
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws LoaderError|RuntimeError|SyntaxError
+     */
+    public function __invoke(Request $request): Response
+    {
+        $response = new Response();
+        $response->prepare($request);
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->set('X-Request-Time', \date_create()->format(\DateTime::ATOM));
+
+        $serializerContext = [JsonEncode::OPTIONS => JSON_PRETTY_PRINT];
+
+        $response->setContent($this->twig->render('index.html.twig', [
+            'env_vars' => $this->serializer->serialize($this->makeEnvVars(), 'json', $serializerContext),
+            'server' => $this->serializer->serialize($request->server, 'json', $serializerContext),
+            'post' => $this->serializer->serialize($request->request, 'json', $serializerContext),
+            'get' => $this->serializer->serialize($request->query, 'json', $serializerContext),
+            'headers' => $this->serializer->serialize($response->headers, 'json', $serializerContext),
+            'quotation' => self::QUOTE,
+        ]));
+
+        return $response;
+    }
+
+    /**
+     * @return array
+     */
+    private function makeEnvVars(): array
+    {
+        return \array_filter(\getenv(), fn(string $key) => \strpos($key, 'APP') === 0, ARRAY_FILTER_USE_KEY);
+    }
+}
